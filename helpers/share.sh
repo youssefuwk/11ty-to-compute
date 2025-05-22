5ae4ed547da4238b5a93161653b82eb2dac4748a
Sue Smith <sue@benormal.info>
2025-05-22 21:10:53 +0100

    Update share.sh

--- helpers/share.sh
+++ helpers/share.sh
@@ -7,7 +7,7 @@ do
     read -ra entries <<<"$i"
     code=$(curl -s -o /dev/null -w "%{http_code}" ${entries[-1]})
     if [[ ${code} -lt 400 ]] ; then
-        echo "📣 Share your website preview by copying this URL: ${entries[-1]}"
+        printf "\n📣 Share your website preview by copying this URL: ${entries[-1]}\n\n"
         break
     fi
 done
